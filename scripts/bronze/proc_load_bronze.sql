3a89c12a4a1c3fbb3de00fd5c65780e697130499
dhayanithi-k-10 <dhayanithik.10@gmail.com>
2026-06-29 11:22:38 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -27,10 +27,10 @@ BEGIN TRY
 		 PRINT'>> -------------';
 
 
-		 SET @start_date =GETDATE();;
+		 SET @start_date =GETDATE();
 		 PRINT'>>TRUNCATING TABLE:'+'bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
-		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_cust_info';
+		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
 		FROM 'D:\SQL BY BAARA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH(
@@ -45,7 +45,7 @@ BEGIN TRY
 		   SET @start_date =GETDATE();
 		 PRINT'>>TRUNCATING TABLE:'+'bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
-		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_cust_info';
+		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
 		FROM 'D:\SQL BY BAARA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH(
@@ -64,7 +64,7 @@ BEGIN TRY
 		  SET @start_date =GETDATE();
 		 PRINT'>>TRUNCATING TABLE:'+'bronze.erp_cust_az12';
 		TRUNCATE TABLE bronze.erp_cust_az12;
-		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_cust_info';
+		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
 		FROM 'D:\SQL BY BAARA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH(
@@ -79,7 +79,7 @@ BEGIN TRY
 		  SET @start_date =GETDATE();
 		 PRINT'>>TRUNCATING TABLE:'+'bronze.erp_loc_a101';
 		TRUNCATE TABLE bronze.erp_loc_a101;
-		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_cust_info';
+		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
 		FROM 'D:\SQL BY BAARA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH(
@@ -94,7 +94,7 @@ BEGIN TRY
 		   SET @start_date =GETDATE();
 		 PRINT'>>TRUNCATING TABLE:'+'bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.crm_cust_info';
+		 PRINT'INSERTING DATA INTO TABLE:'+'bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
 		FROM 'D:\SQL BY BAARA\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH(
@@ -123,3 +123,6 @@ END TRY
 		 PRINT'===========================================';
 	 END CATCH
  END;
+ 
+
+ 
